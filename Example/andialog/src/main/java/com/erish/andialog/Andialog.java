8d2ce4430b821e747934ab39e7be6a2a413b424a
erish <erish2150@gmail.com>
2015-12-09 19:53:09 +0900

    Andialog example created

---
+++ Example/andialog/src/main/java/com/erish/andialog/Andialog.java
@@ -0,0 +1,14 @@
+package com.erish.andialog;
+
+import android.content.Context;
+
+/**
+ * Created by EricShin on 15. 12. 9..
+ */
+public class Andialog {
+    private Context context;
+
+    public Andialog(Context context) {
+        this.context = context;
+    }
+}
